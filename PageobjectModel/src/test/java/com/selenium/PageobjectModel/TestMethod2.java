35940e1fadaca3c79f83ad60bbc6a8525946a2f8
Lenovo <Lenovo@Lenovo-PC>
2018-04-10 20:05:44 +0530

    commit

---
+++ PageobjectModel/src/test/java/com/selenium/PageobjectModel/TestMethod2.java
@@ -0,0 +1,18 @@
+package com.selenium.PageobjectModel;
+
+import org.testng.annotations.Test;
+
+public class TestMethod2 {
+	
+	@Test
+	public void testCase1(){
+		ReadExcelFileExample obj = new ReadExcelFileExample();
+		obj.main(null);
+	}
+	@Test
+	public void testCase2(){
+		ReadExcelFileExample obj = new ReadExcelFileExample();
+		obj.main(null);
+	}
+
+}
